e94c1216a1dd46b35cbb7ed95b83fea9c4912002
matheus-ferreira1 <matheus.fer33@gmail.com>
2026-03-06 17:12:35 -0300

    initial commit with project scaffold

---
+++ app/pages/index.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import type { ButtonProps } from '@nuxt/ui';
+
+const title = 'FinTrack - Your Personal Finance Companion'
+const description
+  = 'Track income, manage expenses, and predict your financial future with our intuitive expense tracker.'
+
+useSeoMeta({
+  titleTemplate: '',
+  title,
+  ogTitle: title,
+  description,
+  ogDescription: description
+})
+
+const links: ButtonProps[] = [
+  {
+    label: 'Start Free Today',
+    to: '/register',
+    trailingIcon: 'material-symbols:arrow-right-alt'
+  },
+  {
+    label: 'Sign In',
+    to: '/login',
+    variant: 'outline'
+  }
+]
+</script>
+
+<template>
+  <div>
+    home
+  </div>
+</template>
+
