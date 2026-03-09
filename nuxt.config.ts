be274c1ab09886fa13485222a0957c29b5c94c1f
matheus-ferreira1 <matheus.fer33@gmail.com>
2026-03-09 19:53:55 -0300

    chore(config): set dev server port to 3003

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   modules: ['@nuxt/ui', 'nuxt-auth-utils', '@nuxt/eslint'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  devServer: {
+    port: 3003,
+  },
   compatibilityDate: '2025-07-15',
   eslint: {
     config: {
