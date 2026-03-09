2cf0a9ac708c8a4d8b2fac6247a6b1146c788668
matheus-ferreira1 <matheus.fer33@gmail.com>
2026-03-09 16:23:47 -0300

    feat(confirmDialog): add ConfirmDialog component and useConfirmDialog composable for modal handling

---
+++ app/components/ConfirmDialog.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+interface ConfirmDialogProps {
+  title?: string
+  description?: string
+}
+
+defineProps<ConfirmDialogProps>()
+
+const emits = defineEmits<{
+  close: [value: boolean]
+}>()
+</script>
+
+<template>
+  <UModal
+    :title="title"
+    :description="description"
+    :dismissible="false"
+    :ui="{ footer: 'justify-end' }"
+  >
+    <template #footer>
+      <UButton
+        label="Cancel"
+        variant="outline"
+        @click="emits('close', false)"
+      />
+      <UButton
+        label="Confirm"
+        @click="emits('close', true)"
+      />
+    </template>
+  </UModal>
+</template>
